37cc029e00408afb98fc5a38d99d0bcb21d28d81
Jon Mico <jonmico@gmail.com>
2024-07-13 10:16:09 -0400

    Add user model.

---
+++ server/src/models/user.ts
@@ -0,0 +1,9 @@
+import mongoose, { Schema } from 'mongoose';
+
+const userSchema = new Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true },
+});
+
+export const User = mongoose.model('User', userSchema);
